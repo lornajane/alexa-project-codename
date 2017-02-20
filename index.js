fa112024fe9a2220145dee80e133e38d47709b40
Lorna Jane Mitchell <lorna@lornajane.net>
2017-02-20 17:25:20 +0000

    Format respose for Alexa

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ function main(args) {
 
   console.log(random);
 
-  return({payload: "OK"});
+  var response = {
+    "version": "1.0",
+    "response" :{
+      "shouldEndSession": true,
+      "outputSpeech": {
+        "type": "PlainText", 
+        "text": "project codename. " + random
+      }
+    }
+  }
+
+  console.log(response);
+
+  return(response);
 }
 
 exports.main = main;
