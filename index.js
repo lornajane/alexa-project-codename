705f4574e1e0558530787a2425f01a3a6f67614d
Lorna Jane Mitchell <lorna@lornajane.net>
2017-06-26 22:23:41 +0100

    Add the option to repeat previous project names

--- index.js
+++ index.js
@@ -4,31 +4,62 @@ var redis = require('redis');
 
 function main(args) {
   console.log(args);
-
-  var pieces = generate().raw;
-  var random = pieces.join(", ");
-  console.log(random);
-  var response = {
-    "version": "1.0",
-    "response" :{
-      "shouldEndSession": true,
-      "outputSpeech": {
-        "type": "PlainText", 
-        "text": "project codename. " + random
+  if(args.request && args.request.intent.name == "RecentCodenames") {
+      if(args.redisURL) {
+        bluebird.promisifyAll(redis.RedisClient.prototype);
+        var client = redis.createClient(args.redisURL);
+        return client.lrangeAsync(["codenames", 0, -1]).then(function (result) {
+          console.log(result);
+          return {
+            "version": "1.0",
+            "response" :{
+              "shouldEndSession": true,
+              "outputSpeech": {
+                "type": "PlainText",
+                "text": result.join(".  ")
+              }
+            }
+          }
+        });
+      } else {
+        return {
+          "version": "1.0",
+          "response" :{
+            "shouldEndSession": true,
+            "outputSpeech": {
+              "type": "PlainText",
+              "text": "I'm sorry, I don't remember"
+            }
+          }
+        }
       }
-    }
-  }
-
-  if(args.redisURL) {
-    bluebird.promisifyAll(redis.RedisClient.prototype);
-    var client = redis.createClient(args.redisURL);
-    return client.lpushAsync(["codenames", random]).then(function (result) {
-      return client.ltrimAsync(["codenames", 0, 2]);
-    }).then(function(result) {
-      return response;
-    });
   } else {
-    return response;
+      // default to generating a new project name
+      var pieces = generate().raw;
+      var random = pieces.join(" ");
+      console.log(random);
+      var response = {
+        "version": "1.0",
+        "response" :{
+          "shouldEndSession": true,
+          "outputSpeech": {
+            "type": "PlainText",
+            "text": "project codename. " + random.replace(' ', ', ')
+          }
+        }
+      }
+
+      if(args.redisURL) {
+        bluebird.promisifyAll(redis.RedisClient.prototype);
+        var client = redis.createClient(args.redisURL);
+        return client.lpushAsync(["codenames", random]).then(function (result) {
+          return client.ltrimAsync(["codenames", 0, 2]);
+        }).then(function(result) {
+          return response;
+        });
+      } else {
+        return response;
+      }
   }
 
 }
